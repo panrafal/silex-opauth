898d751968d7e9059a0ef2138e974bdf33744a7c
Florin Popa <pp.flrn@gmail.com>
2012-09-22 14:43:26 +0300

    Initial structure

---
+++ src/SilexOpauth/OpauthExtension.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace SilexOpauth;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+class OpauthExtension implements ServiceProviderInterface
+{
+    public function boot(Application $app)
+    {
+
+    }
+
+    public function register(Application $app)
+    {
+
+    }
+}
